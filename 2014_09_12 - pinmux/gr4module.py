cc01b087746aa45580db1af63cf7fd0b557798ef
B50002 <B50002@freescale.com>
2014-09-16 17:52:57 +0200

    yerp

---
+++ 2014_09_12 - pinmux/gr4module.py
@@ -0,0 +1,11 @@
+def SHOW_motto(times=1):
+    """Print motto [times]-times!"""
+    str_motto = "Don't make war, make cookies!"
+    print(str_motto)
+#    print("\n".join(str_motto * times))
+
+
+def P__(char="%",width=80):
+    print(char * width)
+if __name__ == "__main__":
+    print("HI THERE AWSOME!")
