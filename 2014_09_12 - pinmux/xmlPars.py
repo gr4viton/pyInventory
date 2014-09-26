2f41fa4411281883680edfc2bec62e7bc9eeda95
B50002 <B50002@freescale.com>
2014-09-26 11:04:23 +0200

    continuous

--- 2014_09_12 - pinmux/xmlPars.py
+++ 2014_09_12 - pinmux/xmlPars.py
@@ -1,18 +1,23 @@
 import xmltodict
 import sys
+import shutil as shu # for copyfile
 
 import linecache
 import re
 #import xml.etree.ElementTree as ET
 import bs4
 import string
-def REPLACE_xml_singalNames(fname, PTs, void_str, fname_new="////"):
+def REPLACE_xml_singalNames(fname, PTs, void_str, fname_new="////", makebackup=True):
     """Goes through [fname] xml file and searches for lines with PTXY in it,
     than it adds 3 to line counter and on this line it replaces the Value tag with the signal name 
     defined in [PTs] dictionary but only if there is not [void_str] in the dictionary."""
 
     if fname_new == "////": 
         fname_new = fname 
+    if fname == fname_new:
+        if makebackup == True:
+            backup_fname = fname+"_BACKUP"
+            shu.copyfile(fname, backup_fname)
     i = 1 # line counter
     i_replace = 0 # line for replacement referencer
     # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -26,6 +31,12 @@ def REPLACE_xml_singalNames(fname, PTs, void_str, fname_new="////"):
 
     # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     #read file
+    min = {}
+    max = {}
+    rep = {}
+    nrep = {}
+    print("%"*42)
+    print("Starting replacement")
     while True:
         line = linecache.getline(fname, i)
         if line:
@@ -79,33 +90,67 @@ def REPLACE_xml_singalNames(fname, PTs, void_str, fname_new="////"):
 
                 try:
                     signal_name = PTs[Pletter][PinNum]
-#                    print("  PTs[%s][%i] = %s" % (Pletter,PinNum,signal_name) )
-                    if signal_name != void_str:
-                        # if the new signal name value is defined
-                        r = regexStr.match(line)
-                        tags = r.groups()
-#                        print( tags )
-                        line = "%s>%s</%s\n" % (tags[0], signal_name, tags[1])
- #                       print( line )
-                        print("PTs[%s][%i] value replaced! sigName= %s" % (Pletter,PinNum, signal_name))
-                    else:
-                        print("PTs[%s][%i] void value! no change" % (Pletter,PinNum))
+                    is_defined = True
                 except:
                     # if this port pin is not defined in PTs input dictionary
                     print("PTs[%s][%i] not defined!" % (Pletter,PinNum))
+                    is_defined = False
+#                    print("  PTs[%s][%i] = %s" % (Pletter,PinNum,signal_name) )
+                if is_defined:
+                    try:
+                        if signal_name != void_str:
+                            # if the new signal name value is defined
+                            r = regexStr.match(line)
+                            tags = r.groups()
+        #                        print( tags )
+                            line = "%s>%s</%s\n" % (tags[0], signal_name, tags[1])
+        #                       print( line )
+                            print("PTs[%s][%i] value replaced! sigName= %s" % (Pletter,PinNum, signal_name))                                             
+                            if not Pletter in rep.keys():   rep[Pletter] = [PinNum]
+                            else:                           rep[Pletter].append(PinNum)
 
+                            if not Pletter in min.keys():
+                                min[Pletter] = PinNum
+                            else:
+                                if min[Pletter] > PinNum:
+                                    min[Pletter] = PinNum
+                            if not Pletter in max.keys():
+                                max[Pletter] = PinNum
+                            else:
+                                if max[Pletter] < PinNum:
+                                    max[Pletter] = PinNum
+                        else:
+                            print("PTs[%s][%i] void value! no change" % (Pletter,PinNum))                                                   
+                            if not Pletter in nrep.keys():  nrep[Pletter] = [PinNum]
+                            else:                           nrep[Pletter].append(PinNum)
+
+                    except IOError as e:
+                        print( "I/O error({0}): {1}".format(e.errno, e.strerror) )
+
+                    
             new_text += line
             i=i+1
         else:
         # end of reading file
             break
     #write file
-    
-    print(fname_new)
+    print("%"*42)
+    if makebackup == True:
+        print("Backup created into file <%s>" % backup_fname)
+    if fname != fname_new:
+        print("Created file <%s> into file <%s>" % (fname, fname_new))
+    else:
+        print("Saved into file <%s>" % fname_new)
     f = open(fname_new,'w')
     f.write(new_text) # python will convert \n to os.linesep
     f.close() # you can omit in most cases as the destructor will call if
-
+    
+    print("%"*42)
+#    print("Replaced these: \nmin:\n%s\nmax:\n%s" % ( str(min), str(max) ) )
+    mima = "\n".join( ["%s[%i-%i]\t=%s" % (port,min[port],max[port],rep[port]) for port in min.keys()] )
+    print("Replaced these: \n%s" % mima)
+    mima = "\n".join( ["%s \t=%s" % (port,nrep[port]) for port in nrep.keys()] )
+    print("Not replaced these: \n%s" % mima)
     print("end")
          
 
